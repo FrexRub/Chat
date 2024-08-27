b26f5ea20e538b21ec0a70f0d391543da43c5a4b
SRubtsov <frexrub@gmail.com>
2024-08-27 13:46:35 +0300

    add test_create_user

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,30 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
+from src.users.crud import create_user
+from src.users.models import User
+from src.users.crud import add_user_to_db
+from src.core.jwt_utils import create_hash_password
+
+USERNAME = "Srub"
+EMAIL = "frexxx@mail.ru"
+PASSWORD = "password"
+
 
 async def test_get_main(client: AsyncClient):
     response = await client.get("/posts")
     assert response.status_code == 307
+
+
+async def test_create_user(db_session):
+    hash_password = create_hash_password(PASSWORD).decode()
+    user: User = User(
+        username=USERNAME,
+        email=EMAIL,
+        hashed_password=hash_password,
+        is_active=True,
+        is_superuser=False,
+    )
+    assert user.username == USERNAME
+    num: int = await add_user_to_db(db_session, user)
+    assert num == 1
